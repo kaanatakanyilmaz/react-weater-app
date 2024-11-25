6691eafb458e94d7b79cb6aca6cd66cd9bfac2a0
Kaan Atakan Yilmaz <yilmazkaanatakan@gmail.com>
2024-11-25 12:16:12 +0300

    App.js be updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import WeaterData from './components/WeatherData';
+import WeatherData from './components/WeatherData';
 import './App.css';
 
 function App() {
   return (
     <div className="bg-blue-400 	grid place-items-center h-screen  font-family: ui-monospace text-3xl font-medium  bg-custom">
-     <div className=' border-y-green-800'>
-     <WeaterData/>
-     </div>
+      <div className=' border-y-green-800'>
+        <div className='text-center '>
+        <h1 className=" text-5xl font-bold mb-6">WEATHER APP</h1>
+        </div>
+        <WeatherData />
+      </div>
     </div>
   );
 }
